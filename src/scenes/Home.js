4aad3cadbbf8d29c3c136eabd39de20242beb5c2
Alex Lee <alexdar94@gmail.com>
2019-01-30 03:47:57 +0800

    Add semantic ui

--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { Header } from 'semantic-ui-react';
 
 export default class Home extends Component {
   render() {
     return (
-      <div>Home</div>
+      <div>
+        <Header as='h1'>First Header</Header>
+      </div>
     );
   }
 }
